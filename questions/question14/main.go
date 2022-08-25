5b34620756507c9a383671b3a8af153cd3a07647
gohsifus <gohsifus@gmail.com>
2022-08-25 18:48:59 +0300

    hotfix

--- questions/question14/main.go
+++ questions/question14/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	slice := []string{"a", "a"}
+	/*slice = make([]string, 2, 16)
+	slice[0] = "a"
+	slice[1] = "a"*/
 	func(slice []string) {
 		slice = append(slice, "a")
 		slice[0] = "b"
