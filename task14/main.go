ff34a735d9820f3768309a1202db5c7e97c79a18
gohsifus <gohsifus@gmail.com>
2022-08-29 13:55:09 +0300

    hotfix task12

--- task14/main.go
+++ task14/main.go
@@ -10,17 +10,14 @@ import (
 int, string, bool, channel из переменной типа interface{}.
 */
 
-func main(){
-	var whoIAm interface{}
-	whoIAm = 23
-
+func checkByTypeAssertion(inp interface{}){
 	//Проверить тип можно с помощью декларации типов
-	if x, ok := whoIAm.(int); ok{
-		fmt.Println(reflect.TypeOf(x))
+	if _, ok := inp.(int); ok {
+		fmt.Println("i am int")
 	}
 
 	//Для удобства можно использовать переключатель типов
-	switch whoIAm.(type){
+	switch inp.(type) {
 	case int:
 		fmt.Println("I am int!")
 	case string:
@@ -30,7 +27,30 @@ func main(){
 	case chan int, chan string, chan interface{}, chan bool:
 		fmt.Println("I am chan")
 	default:
-		//Также тип можно узнать с помощью рефлексии
-		fmt.Println(reflect.TypeOf(whoIAm))
+		fmt.Println("unknown type")
 	}
+}
+
+func checkByReflect(inp interface{}) {
+	inpType := reflect.TypeOf(inp).Kind()
+
+	switch inpType {
+	case reflect.Int:
+		fmt.Println("i am int!")
+	case reflect.String:
+		fmt.Println("i am string!")
+	case reflect.Bool:
+		fmt.Println("i am bool!")
+	case reflect.Chan:
+		fmt.Println("i am channel!")
+	}
+}
+
+func main() {
+	var whoIAm interface{}
+	whoIAm = 23
+
+	checkByTypeAssertion(whoIAm)
+	fmt.Println("**********************")
+	checkByReflect(whoIAm)
 }
